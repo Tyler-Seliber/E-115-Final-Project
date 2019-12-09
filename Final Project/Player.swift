abcaa6d3862c2bd49baf58155c1b4a44515b002d
Tyler Seliber <tylerseliber@gmail.com>
2019-12-09 18:04:19 -0500

    Added player object and some attributes

---
+++ Final Project/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Final Project
+//
+//  Created by Tyler Seliber on 12/9/19.
+//  Copyright © 2019 Tyler Seliber. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+    var playerColor = ""
+    var score = 0
+    var isTurn = false
+    
+}
